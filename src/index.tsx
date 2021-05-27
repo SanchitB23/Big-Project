b61b505bc2357a496a093b1e5494bb8c7817e3de
SanchitB23 <sanbht23@gmail.com>
2021-05-27 22:03:45 +0530

    Added unpkg plugin to handle multiple file fetching in code (handling imports correctly)

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,58 @@
 import ReactDOM from "react-dom";
-import App from "./App";
+import {useEffect, useRef, useState} from "react";
+import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
+import * as esbuild from "esbuild-wasm";
+
+const App = () => {
+    const ref = useRef<any>()
+
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const onClick = async () => {
+        if (!ref.current) return
+
+        /*
+                const result = await ref.current.transform(input, {
+                    loader: 'jsx',
+                    target: 'es2015'
+                })
+        */
+
+        /*added plugin to handle import files*/
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
+
+        })
+        // setCode(result)
+        setCode(result.outputFiles[0].text)
+    }
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+    useEffect(() => {
+        startService().then(() => console.log("Service Started"))
+    }, [])
+    return (
+        <div>
+            <textarea value={input} onChange={event => setInput(event.target.value)}/>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    )
+}
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
