a776bc1c3853ae662fa2274da2c4312bd64cdbf8
SanchitB23 <sanbht23@gmail.com>
2021-05-31 22:28:04 +0530

    Added ability to process user code from textarea and display output in iframe

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,22 @@ import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
 import * as esbuild from "esbuild-wasm";
 import {fetchPlugin} from "./plugins/fetch-plugin";
 
+
 const App = () => {
     const ref = useRef<any>()
+    const iframe = useRef<any>()
 
     const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+        })
+    }
+    useEffect(() => {
+        startService().then(() => console.log("Service Started"))
+    }, [])
 
     const onClick = async () => {
         if (!ref.current) return
@@ -19,6 +30,8 @@ const App = () => {
                     target: 'es2015'
                 })
         */
+
+        iframe.current.srcdoc = html;
 
         /*added plugin to handle import files*/
         const result = await ref.current.build({
@@ -35,25 +48,42 @@ const App = () => {
             }
 
         })
-        setCode(result.outputFiles[0].text)
+
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*")
+
     }
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        })
-    }
-    useEffect(() => {
-        startService().then(() => console.log("Service Started"))
-    }, [])
+    const html = `
+        <html lang="en">
+            <head>
+                <title>Test Sub HTML</title>
+            </head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message',(event) => {
+                        try {
+                          eval(event.data);
+                        } catch (e) {
+                          const root = document.querySelector('#root');
+                          root.innerHTML = '<div style="color:red;">'+ e +'</div>'
+                          console.error("Inner Code Error "+e)
+                        }
+                    },false)
+                </script>
+            </body>
+        </html>     
+    `
+
     return (
         <div>
-            <textarea value={input} onChange={event => setInput(event.target.value)}/>
+            <textarea value={input} onChange={event => {
+                setInput(event.target.value)
+            }}/>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            <iframe ref={iframe} srcDoc={html} title="Preview" sandbox="allow-scripts"/>
         </div>
     )
 }
